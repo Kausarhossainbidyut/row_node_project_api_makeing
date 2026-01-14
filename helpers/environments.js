b6f8a966377ee0bf0bc1ea381bfa78e407a80854
KH.Bidyut <khbidyut31@gmail.com>
2026-01-14 22:17:50 +0600

    learn enveronments veriables

---
+++ helpers/environments.js
@@ -0,0 +1,31 @@
+/* ====================================
+ * Title       : Environments
+ * Description : Environments related things
+ * Author      : Md Kausar Hossain Bidyut
+ * Date        : 2026-01-14
+ =================================== */
+
+ //dependencies
+
+
+ //module scaffolding
+ const environments = {}
+
+ environments.staging={
+    port: 3000,
+    envName: 'staging'
+ }
+
+ environments.production = {
+    port: 5000,
+    envName: 'production'
+ }
+
+ // determine which environments was passed
+ const currentEnvironment = typeof(process.env.NODE_ENV)==='staging' ? process.env.NODE_ENV : 'staging'
+
+ //export corresponding environment object
+ const environmentToExport = typeof(environments[currentEnvironment])=== 'object' ? environments[currentEnvironment] : environments.staging
+
+ //export module
+ module.exports = environmentToExport
